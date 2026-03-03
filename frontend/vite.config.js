bdf0d4abc21b91ff5ad7a795a1b698e6876bf000
Eve <xesxwindlle@gmail.com>
2026-03-03 15:20:17 -0800

    <fix> Took donw custom base URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/readdit.com/",
   plugins: [
     vue(),
     vueDevTools(),
