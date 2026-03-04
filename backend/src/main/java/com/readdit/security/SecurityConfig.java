409c9073cf5267683db516ca7238c574ecc4f610
Eve <xesxwindlle@gmail.com>
2026-03-03 18:07:04 -0800

    <fix> Allow requests from all orgins

--- backend/src/main/java/com/readdit/security/SecurityConfig.java
+++ backend/src/main/java/com/readdit/security/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
